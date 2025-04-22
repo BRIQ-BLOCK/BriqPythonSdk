1851a1509cfaedfdf21f43485e8b2f0dd96cba03
eddiegulay <edgargulay@outlook.com>
2025-04-22 15:53:44 +0200

    fix: Update version to 0.1.2 in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="briq",
-    version="0.0.9",
+    version="0.1.2",
     author="Briq Team",
     author_email="sms@briq.tz",
     description="Python client for the Briq messaging platform API",
